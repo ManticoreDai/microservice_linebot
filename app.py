a47f7b79c615aec1324948e0511e3f69c37b74bb
ManticoreDai <a136724262@gmail.com>
2021-12-12 19:05:44 +0800

    init

---
+++ app.py
@@ -0,0 +1,159 @@
+from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort #, session
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+
+import configparser
+
+import random
+import json
+
+import json_process
+import re
+import pandas as pd
+
+app = Flask(__name__)
+#app.config['SECRET_KEY'] = '123456'
+
+# LINE 聊天機器人的基本資料
+try:
+    # set in Heroku
+    line_access_token = os.environ['line_access_token']
+    line_secret = os.environ['line_secret']
+except:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    line_access_token = config.get('line-bot', 'line_access_token')
+    line_secret = config.get('line-bot', 'line_secret')
+
+line_bot_api = LineBotApi(line_access_token)
+handler = WebhookHandler(line_secret)
+
+
+movies_df = pd.read_excel('./movies_info.xlsx')
+print(movies_df)
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    
+    try:
+        print(body, signature)
+        handler.handle(body, signature)
+        
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# #此段可藉由user回覆LineBot後，便可取得user_id，有user_id，就能主動推撥(Push)訊息給user(尚未成熟)
+# def get_id(event):
+#     user_id = event.source.user_id
+#     print("user_id =", user_id)
+
+#     try:
+#         line_bot_api.push_message(user_id, TextSendMessage(text="請輸入1~20的數字，查詢電影排行榜"))
+#     except Exception as e:
+#         print(e)
+
+@handler.add(MessageEvent, message=TextMessage)
+def movies(event):
+    
+    value_rank = re.compile(r'^排行榜 *(2[0]|1[0-9]|[1-9])$')
+    result_rank = value_rank.match(event.message.text)
+    if result_rank:
+        id = eval(event.message.text[3:])
+        movies_dict = get_movie(movies_df, rank = id) 
+        json_process.rank(movies_dict, id)
+        FlexMessage = json.load(open('rank.json','r',encoding='utf-8'))
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage('Movie Introduction',FlexMessage))
+
+    #value_theater = re.compile(r'^排行榜 *(2[0]|1[0-9]|[1-9])$')
+    if event.message.text == "電影院屏東":
+        
+        FlexMessage = json.load(open('theater_Pingtung.json','r',encoding='utf-8'))
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage('Theater Location',FlexMessage))
+        
+    elif event.message.text == "電影院高雄":
+        FlexMessage = json.load(open('theater_Kaohsiung.json','r',encoding='utf-8'))
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage('Theater Location',FlexMessage))
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    id = int(event.postback.data)
+    if id > len(movies_df):
+        id = len(movies_df)
+    # poster(movies_df['劇照'][id])
+    TemplateMessage = json.load(open('poster.json','r',encoding='utf-8'))
+    image_url=eval(movies_df['劇照'][id-1])[0]
+    print("在這裡:"+image_url)
+
+    if len(eval(movies_df['劇照'][id-1])) != 0:
+        carousel_template = ImageCarouselTemplate(
+                columns=[
+                    ImageCarouselColumn(
+                        image_url=eval(movies_df['劇照'][id-1])[0],
+                        action=MessageTemplateAction(
+                            label=movies_df['片名'][id-1],
+                            text="劇照"
+                        )
+                    ),
+                    ImageCarouselColumn(
+                        image_url=eval(movies_df['劇照'][id-1])[1],
+                        action=MessageTemplateAction(
+                            label=movies_df['片名'][id-1],
+                            text="劇照"
+                        ),
+                    ),
+                    ImageCarouselColumn(
+                        image_url=eval(movies_df['劇照'][id-1])[2],
+                        action=MessageTemplateAction(
+                            label=movies_df['片名'][id-1],
+                            text="劇照"
+                        ),
+                    ),
+                    ImageCarouselColumn(
+                        image_url=eval(movies_df['劇照'][id-1])[3],
+                        action=MessageTemplateAction(
+                            label=movies_df['片名'][id-1],
+                            text="劇照"
+                        ),
+                    ),
+                    ImageCarouselColumn(
+                        image_url=eval(movies_df['劇照'][id-1])[4],
+                        action=MessageTemplateAction(
+                            label=movies_df['片名'][id-1],
+                            text="劇照"
+                        ),
+                    )]
+                )
+
+    line_bot_api.reply_message(event.reply_token, TemplateSendMessage(alt_text="電影劇照", template=carousel_template)) #TemplateSendMessage('profile',TemplateMessage)
+
+def get_movie(df ,rank = 1, col = None):
+   if rank > len(df):
+       rank = len(df)
+   print(f'rank = {rank}, len(df) = {len(df)}')
+   if col == None: 
+       res = {}
+       res = df.iloc[rank-1].to_dict()
+       return res
+   if col in ("本周排名", "片名","評分","連結",'電影海報',"上映日期","片長","發行公司","導演","劇情介紹"): 
+       res = str(df.iloc[rank-1][col])
+       return  res
+   else: print("無此資訊")
+
+
+# line_bot_api.push_message("Ub7385e82f5a34b097e8a12ec38601723", TextSendMessage(text="請輸入1~20的數字，查詢電影排行榜"))
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+
+#
---
+++ json_process.py
@@ -0,0 +1,137 @@
+#https://blog.csdn.net/hjxu2016/article/details/79109407
+#https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/356901/
+
+import json
+
+def rank(data_dict, index):
+    test = './rank.json'
+    #劇情介紹不能超過300字
+    if len(data_dict['劇情介紹']) > 299:
+        data_dict['劇情介紹'] = data_dict['劇情介紹'][0:295] + "..."
+
+
+    temp_dict = {} # template_dict
+    temp_dict["type"] = "bubble"
+    temp_dict["hero"] = {
+        "type": "image",
+        "url": data_dict['電影海報'],
+        "size": "full",
+        "aspectRatio": "3.95:5.6"}
+    temp_dict["body"] = {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+            "type": "text",
+            "text": data_dict['片名'],
+            "size": "lg",
+            "weight": "bold"
+            },
+            {
+            "type": "text",
+            "text": "上映日期：" + data_dict['上映日期']
+            },
+            {
+            "type": "text",
+            "text": "片　　長：" + data_dict['片長']
+            },
+            {
+            "type": "text",
+            "text": "發行公司："+ data_dict['發行公司']
+            },
+            {
+            "type": "text",
+            "text": "導　　演：" + data_dict['導演']
+            },
+            {
+            "type": "button",
+            "action": {
+                "type": "message",
+                "label": "劇情介紹",
+                "text": data_dict['劇情介紹'] #不能大於300
+            },
+            "color": "#FF6B6E",
+            "style": "primary",
+            "margin": "lg"
+            },
+            {
+            "type": "button",
+            "action": {
+                "type": "uri",
+                "label": "更多資訊",
+                "uri": data_dict['連結']
+            },
+            "color": "#A17DF5",
+            "style": "primary",
+            "margin": "lg"
+            }
+        ]
+        }
+
+    if len(eval(data_dict['劇照'])) != 0:
+        temp_dict["body"] = {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                "type": "text",
+                "text": data_dict['片名'],
+                "size": "lg",
+                "weight": "bold"
+                },
+                {
+                "type": "text",
+                "text": "上映日期：" + data_dict['上映日期']
+                },
+                {
+                "type": "text",
+                "text": "片　　長：" + data_dict['片長']
+                },
+                {
+                "type": "text",
+                "text": "發行公司："+ data_dict['發行公司']
+                },
+                {
+                "type": "text",
+                "text": "導　　演：" + data_dict['導演']
+                },
+                {
+                "type": "button",
+                "action": {
+                    "type": "message",
+                    "label": "劇情介紹",
+                    "text": data_dict['劇情介紹'] #不能大於300
+                },
+                "color": "#FF6B6E",
+                "style": "primary",
+                "margin": "lg"
+                },
+                {
+                "type": "button",
+                "action": {
+                    "type": "uri",
+                    "label": "更多資訊",
+                    "uri": data_dict['連結']
+                },
+                "color": "#A17DF5",
+                "style": "primary",
+                "margin": "lg"
+                },
+                {
+                "type": "button",
+                "action": {
+                    "type": "postback", #
+                    "label": "劇照",
+                    "data": index
+                    #"uri": data_dict['連結']
+                },
+                "color": "#66CDAA",
+                "style": "primary",
+                "margin": "lg"
+                }
+            ]
+            }
+
+
+    with open(test,'w', encoding='utf-8') as f:
+        json.dump(temp_dict,f,ensure_ascii=False)
